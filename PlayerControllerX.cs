e078f40db9465ef6924901ced0f96fc96ec15779
bigfoot_is_real <50281485+edgar433@users.noreply.github.com>
2022-12-20 22:20:46 -0800

    Unit 2 Unity

---
+++ PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timer;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timer > 2)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Debug.Log(timer);
+
+            timer = 0;
+        }
+    }
+}
---
+++ SpawnManagerX.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float spawnMostWait = 5.0f;
+    private float spawnLeastWait = 3.0f;
+    public int startWait;
+    private float spawnWait;
+    public bool stop;
+
+    int randBallPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+    void Update()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+    }
+    // Spawn random ball at random x position at top of play area
+    IEnumerator waitSpawner() 
+    {
+        yield return new WaitForSeconds(startWait);
+        while (!stop)
+        {
+            // Generate random ball index and random spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+            randBallPrefabs = Random.Range(0, 3);
+            //Quaternion spawnRotation = Quaternion.Euler(0, 0, 0);
+            // instantiate ball at random spawn location
+            Instantiate(ballPrefabs[randBallPrefabs], spawnPos, ballPrefabs[randBallPrefabs].transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+
+}
